9aa309f06cd462e477c1a45ee6e1bbdeebb7d172
Vidyapreethi <vparthas@hawk.iit.edu>
2015-11-09 17:59:31 -0600

    Changed Db instance

--- setup.php
+++ setup.php
@@ -19,7 +19,7 @@ $result = $rds->createDBInstance([
    # 'CopyTagsToSnapshot' => true || false,
    # 'DBClusterIdentifier' => '<string>',
     'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'pvp-db', // REQUIRED
+    'DBInstanceIdentifier' => 'pvp-db-mp1', // REQUIRED
     'DBName' => 'customerrecords',
     #'DBParameterGroupName' => '<string>',
     #'DBSecurityGroups' => ['<string>', ...],
@@ -52,11 +52,11 @@ $result = $rds->createDBInstance([
 ]);
 print "Create RDS DB results: \n";
 # print_r($rds);
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'pvp-db',
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'pvp-db-mp1',
 ]);
 // Create a table 
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'pvp-db',
+    'DBInstanceIdentifier' => 'pvp-db-mp1',
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
