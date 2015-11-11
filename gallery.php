69a1d200ac11bf4c741fab475288215a7c62e4db
Vidyapreethi <vparthas@hawk.iit.edu>
2015-11-11 00:19:10 -0600

    Both upload and retrieve functionality tested

--- gallery.php
+++ gallery.php
@@ -9,30 +9,24 @@ $email = $_POST["email"];
 echo $email;
 require 'vendor/autoload.php';
 
-use Aws\Rds\RdsClient;
-$client = RdsClient::factory(array(
-'region'  => 'us-east-1'
-));
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
 
-$result = $client->describeDBInstances(array(
+// Create a table 
+$result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'pvp-db-mp',
-));
+]);
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+print "============\n". $endpoint . "================\n";
 
-$endpoint = "";
+$link = mysqli_connect($endpoint,"controller","ilovebunnies","customerrecords") or die("Error " . mysqli_connect_error($link));  
 
-foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
-    // Do something with the message
-    echo "============". $ep . "================";
-    $endpoint = $ep;
-}   
-//echo "begin database";
-$link = mysqli_connect($endpoint,"controller","ilovebunnies","pvp-db-mp") or die("Error " . mysqli_error($link));
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
 
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
 $link->real_query("SELECT * FROM items WHERE email = '$email'");
@@ -40,8 +34,10 @@ $link->real_query("SELECT * FROM items WHERE email = '$email'");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
-    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
-echo $row['id'] . "Email: " . $row['email'];
+    
+    echo "<img src =\" " . $row['s3rawurl'] . "\" />";
+    //echo "<img src =\"" .$row['s3finishedurl'] . "\"/>";#Finished URL not set
+    echo "<p>".$row['id'] . "Email: " . $row['email']."</p>";
 }
 $link->close();
 ?>
--- setup.php
+++ setup.php
@@ -20,18 +20,37 @@ print "============\n". $endpoint . "================\n";
 
 $link = mysqli_connect($endpoint,"controller","ilovebunnies","customerrecords") or die("Error " . mysqli_connect_error($link));  
 #echo "Here is the result: " . $link;
-$sql = "CREATE TABLE comments 
+$sql_comments = "CREATE TABLE comments 
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 PosterName VARCHAR(32),
 Title VARCHAR(32),
 Content VARCHAR(500)
 )";
-$retVal = $link->query($sql);
-if($retVal === TRUE) {
-print "Table Created";
+$retVal1 = $link->query($sql_comments);
+if($retVal1 === TRUE) {
+print "Comments Table Created";
 } else {
-print "Could not create a table";
+print "Could not create comments table";
+}
+
+$sql_items = "CREATE TABLE items 
+(
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+uname Varchar(20),
+email Varchar(80), 
+phone Varchar(20), 
+s3rawurl Varchar(256), 
+s3finishedurl Varchar(256), 
+filename Varchar(256), 
+status TinyInt(3), 
+issubscribed TinyInt(3)
+)";
+$retVal2 = $link->query($sql_items);
+if($retVal2 === TRUE) {
+print "Items Table Created";
+} else {
+print "Could not create Items table";
 }
 
 $link->close();
