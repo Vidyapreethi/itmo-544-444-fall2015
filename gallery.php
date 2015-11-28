ee89fe885a50938257a80518e3bf7f99c3876ac2
Vidyapreethi <vparthas@hawk.iit.edu>
2015-11-27 22:58:22 -0600

    Removed errors from fotorama jquery plugin

--- gallery.php
+++ gallery.php
@@ -8,11 +8,13 @@
   <link href="fotorama.css" rel="stylesheet">
   <script src="fotorama.js"></script>
 </head>
+
 <body>
 
 <?php
 session_start();
 $email = $_POST["email"];
+echo "Entered email:  ";
 echo $email;
 require 'vendor/autoload.php';
 
@@ -30,8 +32,9 @@ $result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'pvp-db-mp',
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+echo "<p/>";
 print "============\n". $endpoint . "================\n";
-
+echo "<p/>";
 $link = mysqli_connect($endpoint,"controller","ilovebunnies","customerrecords") or die("Error " . mysqli_connect_error($link));  
 
 
@@ -39,15 +42,18 @@ $link = mysqli_connect($endpoint,"controller","ilovebunnies","customerrecords") 
 $link->real_query("SELECT * FROM items WHERE email = '$email'");
 //$link->real_query("SELECT * FROM items");
 $res = $link->use_result();
-echo "Result set order...\n";
+echo "<p/>";
+echo "Result set order:\n";
 
+echo "<div class=\"fotorama\" data-width=\"700\" data-ratio=\"700/467\" data-max-width=\"100%\">";
 while ($row = $res->fetch_assoc()) {
-   <div class="fotorama" data-width="700" data-ratio="700/467" data-max-width="100%"> 
-    echo "<img src =\" " . $row['s3rawurl'] . "\" />";
-</div>
+    
+  echo "<img src =\" " . $row['s3rawurl'] . "\" />";
+
     //echo "<img src =\"" .$row['s3finishedurl'] . "\"/>";#Finished URL not set
-    echo "<p>".$row['id'] . "Email: " . $row['email']."</p>";
+  //  echo "<p>".$row['id'] . "Email: " . $row['email']."</p>";
 }
+ echo "</div>";
 
 $link->close();
 ?>
