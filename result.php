1d0dd2dcea693170ecb68f27287d6a75aa24c073
Vidya Parthasarathy <vparthas@hawk.iit.in>
2015-11-28 22:49:38 -0600

    Adding Imagick Test file

--- result.php
+++ result.php
@@ -6,6 +6,9 @@ session_start();
 echo $_POST['useremail'];
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+$testimage=new Imagick('images/IIT_Scarlet_Hawks.svg.png');
+
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "File is valid, and was successfully uploaded.\n";
