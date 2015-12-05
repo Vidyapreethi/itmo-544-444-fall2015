31ed8ae2eeb22889a8a4e5305f289057a416f22a
Vidyapreethi <vparthas@hawk.iit.edu>
2015-12-04 20:12:56 -0600

    Updated code in account.php

--- account.php
+++ account.php
@@ -1,3 +1,14 @@
 <?php
-header('Location: /upload.php');
+session_start();
+
+include 'header.php';
+
+echo "<h3 center>";
+echo "UserName is  " . $_SESSION["uname"] . ".<br>";
+echo "Email is " . $_SESSION["email"] . ".<br>";
+echo "Phone number is  " . $_SESSION["phone"] . ".<br>";
+echo "</h3>";
+
+include 'footer.php';
+
 ?>
